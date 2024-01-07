143d9615cc005bbf614ecaa230c81e3ea918a074
peetuyeencodedao <nptuyen2801@gmail.com>
2024-01-07 14:16:38 +0700

    first commit

---
+++ application/views/frontend/components/gioithieu/index.php
@@ -0,0 +1,33 @@
+
+<div class=container>
+<h1><center>Giới thiệu Danh Vinh Shop</h1>
+<h3>Giới thiệu chung</h3>
+<a>Công ty Cổ phần Xây dựng và Đầu tư thương mại Hoàng Hà sở hữu chuỗi cửa hàng Hoàng Hà Mobile - là nhà bán lẻ hàng đầu, chuyên cung cấp các sản phẩm công nghệ chính hãng tại thị trường Việt Nam. Năm 1996, Hoàng Hà Mobile được thành lập, từng bước trở thành địa chỉ đáng tin cậy của người tiêu dùng Việt. Với khẩu hiệu “Nếu những gì chúng tôi không có, nghĩa là bạn không cần”, chúng tôi đã, đang và sẽ tiếp tục nỗ lực đem đến các sản phẩm công nghệ chính hãng đa dạng, phong phú đi kèm mức giá tốt nhất phục vụ nhu cầu của quý khách hàng</a>
+<br>Sau hơn 20 năm phát triển, Hoàng Hà Mobile đã trở thành cái tên không còn xa lạ với người tiêu dùng trong nước. Hiện nay chúng tôi đang sở hữu mạng lưới hơn 100 chi nhánh phủ trên khắp cả nước, trong đó bao gồm hai trung tâm bảo hành tại Hà Nội và một trung tâm bảo hành tại thành phố Hồ Chí Minh. Đến với chuỗi cửa hàng của Hoàng Hà Mobile, quý khách có thể hoàn toàn yên tâm về uy tín, chất lượng sản phẩm với mức giá rẻ hơn khoảng 15-20% so với giá bán trên thị trường. Song song với đó, chúng tôi cũng luôn nỗ lực phục vụ đem đến trải nghiệm dịch vụ tốt nhất cho khách hàng. 
+<h3>Những dấu mốc quan trọng</h3>
+<ul>
+<li>Năm 1996: Thành lập cửa hàng Hoàng Hà Mobile đầu tiên tọa lạc trên con phố sầm uất của Thủ đô Hà Nội ở địa chỉ 194 Lê Duẩn.</li>
+<li>Năm 2000: Hoàng Hà Mobile chính thức trở thành nhà phân phối ĐTDĐ chính hãng hợp tác với nhiều nhãn hàng lớn hàng đầu như Samsung, OPPO, Nokia, Huawei,..</li>
+<li>Năm 2006: Với kinh nghiệm 10 năm trong lĩnh vực bán lẻ, Hoàng Hà Mobile phát triển mạnh mẽ và liên tục mở thêm nhiều chi nhánh mới.
+<li>Năm 2016: Kỉ niệm 20 năm hoạt động trong lĩnh vực bán lẻ các sản phẩm công nghệ, Hoàng Hà Mobile đã khẳng định được chỗ đứng vững chắc trên thị trường cũng như trong tiềm thức người tiêu dùng.
+<li>Năm 2019: Hoàng Hà Mobile chính thức hợp tác với ông lớn ngành viễn thông MobiFone Việt Nam mở chuỗi chi nhánh bán hàng liên kết, nâng tổng số chi nhánh tới hơn 60, đồng thời phủ khắp 30 tỉnh thành trên cả nước. Kể từ đó, Hoàng Hà Mobile liên tục đồng hành cùng MobiFone tổ chức các sự kiện lớn nhỏ. 
+<li>Năm 2020: Hoàng Hà Mobile tự hào trở thành nhà bán lẻ ủy quyền chính thức của Apple tại Việt Nam. Các sản phẩm Apple chính hãng VN/A do Apple Việt Nam phân phối được bán tại hệ thống Hoàng Hà Mobile với mức giá tốt nhất thị trường cùng chế độ bảo hành uy tín. 
+</ul>
+<h3>Tôn chỉ hoạt động</h3>
+
+Hoàng Hà Mobile luôn hoạt động dựa trên tôn chỉ đặt khách hàng là trung tâm, mọi nỗ lực để đạt được mục tiêu cao nhất là làm hài lòng người dùng thông qua các sản phẩm được cung cấp và dịch vụ khách hàng. Hoàng Hà Mobile đang từng bước xây dựng dịch vụ khách hàng vượt trội, xứng đáng là đơn vị bán lẻ hàng đầu tại Việt Nam. Sự tin tưởng và ủng hộ nhiệt tình của quý khách hàng tại chuỗi chi nhánh đã phần nào khẳng định hiệu quả hoạt động của đội ngũ nhân viên Hoàng Hà Mobile. 
+
+Đối với quý khách hàng, chúng tôi luôn đặt cái tâm làm gốc, làm việc với tinh thần nghiêm túc, trung thực và có trách nhiệm, để mang tới trải nghiệm dịch vụ tốt nhất. 
+
+Đối với đồng nghiệp, chúng tôi đề cao văn hóa học hỏi, đoàn kết, tương trợ lẫn nhau tạo nên môi trường làm việc tôn trọng - công bằng - văn minh cho nhân viên trong công ty. 
+
+Đối với các đối tác, Hoàng Hà Mobile luôn làm việc dựa trên nguyên tắc tôn trọng, cùng tạo ra giá trị cho cộng đồng và cùng phát triển bền vững.  
+
+<h3>Tầm nhìn và sứ mệnh</h3>
+
+Những năm qua, chúng tôi không ngừng cải thiện dịch vụ tại các chi nhánh và hỗ trợ khách hàng qua các kênh online. Hoàng Hà Mobile cam kết mang đến những sản phẩm chất lượng và chế độ bảo hành uy tín, sẵn sàng hỗ trợ khách hàng trong thời gian nhanh nhất.
+
+Trong tương lai, Hoàng Hà Mobile sẽ tiếp tục mở rộng hệ thống chi nhánh, hướng tới mục tiêu có mặt tại 63 tỉnh thành trên toàn quốc. Đồng thời, nâng cao chất lượng dịch vụ, hạn chế những rủi ro, lắng nghe và tiếp thu góp ý của quý khách hàng nhằm đem đến trải nghiệm tốt nhất khi mua sắm tại Hoàng Hà Mobile. 
+
+Cuối cùng, Hoàng Hà Mobile hy vọng sẽ trở thành nhà tiên phong đưa những sản phẩm công nghệ mới nhất đến tay người dùng sớm nhất, tạo ra cuộc sống hiện đại nơi công nghệ kết nối con người, công nghệ phục vụ con người
+</div>
---
+++ application/views/frontend/modules/footer.php
@@ -0,0 +1,49 @@
+<footer id="footer" style='padding-bottom:80px'>
+    <div class="secction-footer"></div>
+  </div>
+    <div class="container">
+        <div class="col-xs-12 col-sm-4">
+                <a href="trang-chu">
+                    <img src="public/images/v.png" style="width: 80%;" class="img-fix f-logo" alt="smart-construction">
+                </a>
+                <div class="about-store"><b>
+                ㅤUY TÍN - CHẤT LƯỢNG - NHANH CHÓNG</b>
+             </div>
+         </div> 
+             <div class="col-xs-12 col-sm-3 text-left">
+            <div class="f-contact">
+                <h3>Thông tin liên hệ</h3>
+                <ul class="list-unstyled">
+                    <li class="clearfix">
+                        <i class="fa fa-map-marker"></i>
+                        <span>Hưng Dũng - TP Vinh</span>
+                    </li>
+                    <li class="clearfix">
+                        <i class="fa fa-phone"></i>
+                        <span>0366765726</span>
+                    </li>
+                    <li class="clearfix">
+                        <i class="fa fa-envelope"></i>
+                        <span><a href="mailto:alitrumcuoi@gmail.com">alitrumcuoi@gmail.com</a></span>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-3 text-left">
+            <h3 class="footer-title">HỖ TRỢ KHÁCH HÀNG</h3>
+            <ul class="list-unstyled linklists">
+                <li><a href="chinh-sach">Hướng dẫn thanh toán</a></li>          
+                <li><a href="chinh-sach">Hướng dẫn đặt hàng</a></li>
+                <li><a href="dieu-khoan">Điều khoản</a></li>                   
+            </ul>
+        </div>
+        <div class="col-xs-12 col-sm-2" style=margin-top:20px>
+                <ul class="list-unstyled social pull-right">
+                  <li><a href="https://www.facebook.com/AliTV.vn"><i class="fab fa-facebook-f"></i></a></li>
+                  <li><a href="https://twitter.com/VinhPham2222"><i class="fab fa-twitter"></i></a></li>
+                  <li><a href="https://mail.google.com/mail/u/0/#inbox?compose=FZxzwjpjpjcPSFXWbWKvZpFJGXQgsXCXfjdsmkxDDFVSKftBlKFjrNLszCzTdTmjCwszNlCgLxkNbkHRVxzHvQVStVnVqbktwHhJBWFZppCvkdHqfTplQFGJvKlNRbspkbPqthRJcBhrjqXRhvVgnMKpPjWTBBHmvVljV"><i class="fab fa-google-plus-g"></i></a></li>
+                  <li><a href="https://www.youtube.com/channel/UCL2i4bVAPZ19UjgeKCjCmmA"><i class="fab fa-youtube"></i></a></li>
+              </ul>
+   
+</div>
+</footer>
